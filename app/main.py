f480594f422373cbd03c9ef29aebb77ad7fd3dc7
evycorreia <ecorreia.aguilera@gmail.com>
2023-09-15 15:13:22 -0300

    Revert "este commit no debe ir"
    This reverts commit eb2383c7238751bae00bf7605cfcb2969766eb58.

--- app/main.py
+++ app/main.py
@@ -21,7 +21,6 @@ def run():
     print(country)
     labels, values = utils.get_population(country)
     charts.generate_bar_chart(country['Country'], labels, values)
-    asdfasdfasdf
 
 if __name__ == '__main__':
   run()
